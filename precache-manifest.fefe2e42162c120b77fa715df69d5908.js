340a1c9f7394afc356d893c4cabd77f10c65d849
sanchocreativo <sanchocreativo@gmail.com>
2022-04-17 18:41:55 -0300

    Updates

---
+++ precache-manifest.fefe2e42162c120b77fa715df69d5908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f6378502d4b0f4cc7b1",
+    "url": "/albumify.app/static/css/main.b3e2f0c4.chunk.css"
+  },
+  {
+    "revision": "9f6378502d4b0f4cc7b1",
+    "url": "/albumify.app/static/js/main.9f637850.chunk.js"
+  },
+  {
+    "revision": "f0bd717f13db6005f25e",
+    "url": "/albumify.app/static/css/1.95bf5611.chunk.css"
+  },
+  {
+    "revision": "f0bd717f13db6005f25e",
+    "url": "/albumify.app/static/js/1.f0bd717f.chunk.js"
+  },
+  {
+    "revision": "b1531b793c14ee6b93cc",
+    "url": "/albumify.app/static/js/runtime~main.b1531b79.js"
+  },
+  {
+    "revision": "6e2344e20e9c2a8589a3741d254d5597",
+    "url": "/albumify.app/index.html"
+  }
+];
